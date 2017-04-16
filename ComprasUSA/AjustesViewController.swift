a8622475e9646b4ce5844b0049ae9da6bc6b9a49
rafaelfioretti <rafaelfioretti@hotmail.com>
2017-04-16 20:44:23 -0300

    commit

---
+++ ComprasUSA/AjustesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AjustesViewController.swift
+//  ComprasUSA
+//
+//  Created by Rafael Fioretti on 4/16/17.
+//  Copyright © 2017 RafaelHeitor. All rights reserved.
+//
+
+import UIKit
+
+enum SettingsType: String {
+    case cotacaoDolar = "cotacao_dolar"
+    case vlIOF = "vl_iof"
+}
+
+class AjustesViewController: UIViewController {
+    
+    @IBOutlet weak var etCotacao: UITextField!
+    @IBOutlet weak var etIOF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        etCotacao.text = UserDefaults.standard.string(forKey: SettingsType.cotacaoDolar.rawValue)
+        etIOF.text = UserDefaults.standard.string(forKey: SettingsType.vlIOF.rawValue)
+    }
+}
