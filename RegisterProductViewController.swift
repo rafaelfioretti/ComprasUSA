b74144bad38330f917615dcde9c74c26c9f9433a
Heitor Souza <hsouza@Avenues-MacBook-Pro.local>
2017-04-16 21:39:18 -0300

    Gift Image - camera access

--- RegisterProductViewController.swift
+++ RegisterProductViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class RegisterProductViewController: UIViewController {
+    
+    
+    @IBOutlet weak var giftImage: UIImageView!
+   
+     var smallImage: UIImage!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,49 @@ class RegisterProductViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func selectPicture(sourceType: UIImagePickerControllerSourceType){
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        
+        present(imagePicker, animated:true, completion: nil)
+        imagePicker.delegate = self
+    }
+    
+    
+    @IBAction func postGiftImage(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Selectionar postar", message: "Escolher o poste do filme", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default){
+                (action: UIAlertAction) in
+                self.selectPicture(sourceType: .camera)
+            }
+            
+            alert.addAction(cameraAction)
+        }
+        
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default){
+            (action: UIAlertAction) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        
+        alert.addAction(libraryAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+ 
+    
 
     /*
     // MARK: - Navigation
@@ -32,4 +82,25 @@ class RegisterProductViewController: UIViewController {
     }
     */
 
+}
+
+extension RegisterProductViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        giftImage.image = image
+        
+        let smallSize = CGSize(width: 300, height: 280)
+        UIGraphicsBeginImageContext(smallSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        giftImage.image = smallImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+    
+    }
 }
